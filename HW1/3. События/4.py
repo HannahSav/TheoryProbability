5baeec8b25230ec06082172cc8906cbd8822d367
Galina Savon <g.savon11@yanex.ru>
2023-10-18 13:48:40 +0300

    4 task

---
+++ HW1/3. События/4.py
@@ -0,0 +1,23 @@
+# Created by Hannah at 17.10.2023 23:52
+string = ""
+string += "Беркман"
+string += "Гребеников"
+string += "Карпинский"
+string += "Кирсанова"
+string += "Майданец"
+string += "Пугачев"
+string += "Савон"
+string += "Смакотина"
+string += "Терентьева"
+string += "Трофимов"
+string += "удалова"
+string += "Мирошниченко"
+string += "Тягунова"
+string += "Киреев"
+
+sum_gl = 0
+for i in string:
+    if i in ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']:
+        sum_gl += 1
+
+print("Кол-во гласных:{}, кол-во букв {}, вероятность, {}".format(sum_gl, len(string), sum_gl / len(string)))
