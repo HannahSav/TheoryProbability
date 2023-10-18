04c1ccd6026693f748e60484cc6fbeb9c5440efd
Galina Savon <g.savon11@yanex.ru>
2023-10-18 13:20:29 +0300

    2nd task

---
+++ HW1/3. События/2.py
@@ -0,0 +1,20 @@
+# Created by Hannah at 17.10.2023 23:53
+
+a = []
+for i in range(37):
+    a.append(0)
+
+for i in range(1, 13):
+    for j in range(1, 13):
+        for k in range(1, 13):
+            a[i + j + k] += 1
+
+print("сумма : кол-во наборов")
+sum = 0
+for i in range(2, 25):
+    print(i, ':', a[i])
+    sum += a[i]
+
+print("сумма : вероятность")
+for i in range(2, 25):
+    print(i, ":", a[i] / sum)
