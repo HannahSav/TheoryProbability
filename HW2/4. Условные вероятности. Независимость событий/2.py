23ab0b149e7c3229bb860bb2cc0c71a8378a4fa1
Galina Savon <g.savon11@yanex.ru>
2023-10-19 18:10:39 +0300

    2.3

--- HW2/4. Условные вероятности. Независимость событий/2.py
+++ HW2/4. Условные вероятности. Независимость событий/2.py
@@ -1,2 +1,9 @@
 # Created by Hannah at 19.10.2023 17:10
-
+all_var = 12 ** 3
+need = 0
+for i in range(1, 13):
+    for j in range(1, 13):
+        for k in range(1, 13):
+            if j != i and i != k and j != k and (j == 12 or i == 12 or k == 12):
+                need += 1
+print(all_var, need, need/all_var)
